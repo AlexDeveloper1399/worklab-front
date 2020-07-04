783462ab37410b2960636a7c3cdb032dfa3ce762
Alexander <61645144+AlexDeveloper99@users.noreply.github.com>
2020-07-04 17:05:48 +0600

    first commit

---
+++ src/views/Components/Sections/SectionCarousel.js
@@ -0,0 +1,76 @@
+import React from "react";
+// react component for creating beautiful carousel
+import Carousel from "react-slick";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import LocationOn from "@material-ui/icons/LocationOn";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Card from "components/Card/Card.js";
+
+import image1 from "assets/img/medi.jpg";
+import image2 from "assets/img/law.jpg";
+import image3 from "assets/img/econ.jpg";
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function SectionCarousel() {
+  const classes = useStyles();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false
+  };
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
+            <Card carousel>
+              <Carousel {...settings}>
+                <div>
+                  <img src={image1} alt="First slide" className="slick-image" />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Ты можешь стать врачем и спасать людям жизни
+                    </h4>
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src={image2}
+                    alt="Second slide"
+                    className="slick-image"
+                  />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Ты можешь стать отличным юристом и представлять в своем лице закон
+                    </h4>
+                  </div>
+                </div>
+                <div>
+                  <img src={image3} alt="Third slide" className="slick-image" />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Или можешь стать финансистом и управлять деньгами
+                    </h4>
+                  </div>
+                </div>
+              </Carousel>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
